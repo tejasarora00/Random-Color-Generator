5fee12e0f488dcbf61b5decbbd80e0a8074325fa
Tejas Arora <151165682+tejasarora00@users.noreply.github.com>
2024-11-27 15:08:32 +0530

    Files Changed

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let randomBtn = document.querySelector("button");
 
-randomBtn.addEventListener("click", function () {
-    let h1 = document.querySelector('h1');
-    let randomColor = getRandomColor();
-    h1.innerText = randomColor;
-    let div = document.querySelector('div');
-    div.style.backgroundColor = randomColor;
-})
-
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
+function getRandomColor(){
+    let red = Math.floor(math.random()*255);
+    let green = Math.floor(math.random()*255);
+    let blue = Math.floor(math.random()*255);
     let color = `rgb(${red}, ${green}, ${blue})`;
     return color;
 }
+
+randomBtn.addEventListener("click", function(){
+    let h1 = document.querySelector("h1");
+    let div = document.querySelector("div");
+    let randomColor = randomColor;
+    h1.innerText = randomColor;
+    div.style.backgroundColor = randomColor;
+})
